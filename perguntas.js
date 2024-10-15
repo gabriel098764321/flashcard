a9f09b9db46f4079b87620fde4075da96dd0f3e3
Gabriel Correa <148580196+gabriel098764321@users.noreply.github.com>
2024-10-15 16:04:12 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é a captal da França?',
+    'A captal da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em Inglês é HI (RAI)'
+)
+
+criaCartao(
+    'Biologia',
+    'Quem é considerado o pai da genética?',
+    'Gregor Mendel'
+)
+
+criaCartao(
+    'Matemática',
+    'Qual é o valor de Pi?',
+    '3,14'
+)
